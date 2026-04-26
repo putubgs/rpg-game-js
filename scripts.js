b98c1a95cb38afc2c0b29e32b0b84d7f0277fff5
SeedONova <putubaguswidia@gmail.com>
2026-04-26 19:23:22 +0700

    feat: battle logs

--- scripts.js
+++ scripts.js
@@ -6,6 +6,8 @@ const ultiBtn = document.getElementById("ultimate-button");
 const healBtn = document.getElementById("heal-button");
 const battleLogs = document.getElementById("battle-logs");
 
+const logs = [];
+
 var heroHealth = 100;
 var monsterHealth = 100;
 const heroHealthBox = document.querySelector(".hero-health-bar");
@@ -16,34 +18,24 @@ console.log(heroHealthBox);
 heroHealthBox.style.setProperty("--hero-health", `${heroHealth}%`);
 monsterHealthBox.style.setProperty("--monster-health", `${monsterHealth}%`);
 
-let monsterOpt = 0;
+let monsterOpt = "attack";
 
 // ATTACK FUNCTION AND CURRENT HEALTH CALCULATOR
 const attack = () => {
-  battleLogs.innerHTML = "user attacked";
-
   let lolAttack = generateDamage(Math.round(Math.random() * 1));
-
-  tempMonsterHP -= lolAttack.heroDmg;
-  tempHeroHP -= lolAttack.monsterDmg;
 
   console.log(tempHeroHP);
   console.log(tempMonsterHP);
 
   heroHealth = deathDetector(heroHealth - lolAttack.monsterDmg);
-
   heroHealthBox.style.setProperty("--hero-health", heroHealth + "%");
-
   monsterHealth = deathDetector(monsterHealth - lolAttack.heroDmg);
-
   monsterHealthBox.style.setProperty("--monster-health", `${monsterHealth}%`);
-
-  monsterHealth.innerHTML = deathDetector(
-    monsterHealth.innerHTML - lolAttack.monsterDmg,
-  );
 
   console.log(`Monster's health remaining ${monsterHealth.innerHTML}`);
   console.log(`Hero's health remaning ${heroHealth}`);
+
+  logsGenerator(monsterOpt, "attack", lolAttack.monsterDmg, lolAttack.heroDmg)
 
   if (winnerSelector(monsterHealth, heroHealth) == "hero") {
     console.log("the monster is dead");
@@ -85,6 +77,7 @@ const deathDetector = (HP) => {
   return HP;
 };
 
+// FUNCTION TO GENERATE DAMAGE
 const generateDamage = (monsterOpt, attackOpt = "attack") => {
   let monsterDmg = 0;
   let heroDmg = 0;
@@ -92,16 +85,12 @@ const generateDamage = (monsterOpt, attackOpt = "attack") => {
   console.log(`The monster attack option ${monsterOpt}`);
   if (attackOpt == "ultimate") {
     heroDmg = Math.round(Math.random() * 20 + 5);
-    // console.log(`the ultimeate attack value is ${Math.random() * 20 + 5}`);
   } else {
     heroDmg = Math.round(Math.random() * 10 + 5);
   }
 
-  if (monsterOpt == 2) {
+  if (monsterOpt == "ultimate") {
     monsterDmg = Math.round(Math.random() * 20 + 5);
-    // console.log(
-    // `Monster's ultimate attack value is ${Math.random() * 20 + 5}}`,
-    // );
   } else {
     monsterDmg = Math.round(Math.random() * 10 + 5);
   }
@@ -111,6 +100,32 @@ const generateDamage = (monsterOpt, attackOpt = "attack") => {
     monsterDmg: monsterDmg,
   };
 };
+
+const logsGenerator = (monsterAttOpt, heroAttOpt, monsterDmg, heroDmg) => {
+  console.log(`Monster attack option: ${monsterAttOpt}`);
+  console.log(`Hero attack option: ${heroAttOpt}`);
+  console.log(`Monster damage: ${monsterDmg}`);
+  console.log(`Hero damage: ${heroDmg}`);
+
+  battleLogs.innerHTML = "user attacked";
+
+  let monsterLogMsg = `MONSTER: received ${heroDmg} damage from ${heroAttOpt == "attack" ? "basic attack" : heroAttOpt}`
+  let heroLogMsg = `HERO: received ${monsterDmg} damage from ${monsterAttOpt == "attack" ? "basic attack" : monsterAttOpt}`
+
+  battleLogs.innerHTML += `<p>${monsterLogMsg}</p>`;
+  battleLogs.innerHTML += `<p>${heroLogMsg}</p>`;
+
+  logs.push(monsterLogMsg);
+  logs.push(heroLogMsg)
+
+  renderLogs()
+}
+
+const renderLogs = () =>{
+  battleLogs.innerHTML = logs.map(log => `<p>${log}</p>`).join("")
+
+  battleLogs.scrollTop = battleLogs.scrollHeight;
+}
 
 attackBtn.addEventListener("click", attack);
 ultiBtn.addEventListener("click", ultimate);
